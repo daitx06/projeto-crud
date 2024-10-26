f8cafd89fa8240a5a0f04896930482ac37ebf310
João Vitor Daitx <162595785+daitx06@users.noreply.github.com>
2024-10-25 22:25:37 -0300

    first commit

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -22,32 +22,32 @@
     </div>
 </div>
 
-  @if(session('status'))
+@if(session('status'))
     <div class="alert alert-success mb-1 mt-1">
         {{ session('status') }}
     </div>
-  @endif
+@endif
 
 <form action="{{ route('notes.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
 
-     <div class="row">
+    <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Title:</strong>
                 <input type="text" name="title" class="form-control" placeholder="Title">
-               @error('title')
-                  <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-               @enderror
+            @error('title')
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+            @enderror
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Description:</strong>
-                 <input type="text" name="description" class="form-control" placeholder="Description">
+                <input type="text" name="description" class="form-control" placeholder="Description">
                 @error('description')
-                  <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-               @enderror
+                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+            @enderror
             </div>
         </div>
     <div class="col-xs-6 col-sm-6 col-md-6 mt-2">
--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -21,23 +21,23 @@
         </div>
     </div>
 
-  @if(session('status'))
+@if(session('status'))
     <div class="alert alert-success mb-1 mt-1">
         {{ session('status') }}
     </div>
-  @endif
+@endif
 
     <form action="{{ route('notes.update',$note->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
 
-         <div class="row">
+        <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>note Name:</strong>
                     <input type="text" name="title" value="{{ $note->title }}" class="form-control" placeholder="please enter title">
                     @error('title')
-                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
@@ -47,13 +47,13 @@
                     <strong>Description:</strong>
                     <input type="text" name="description" value="{{ $note->description }}" class="form-control" placeholder="Please enter description">
                     @error('description')
-                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                     @enderror
                 </div>
             </div>
-             <div class="col-xs-6 col-sm-6 col-md-6 mt-2">
-              <button type="submit" class="btn btn-primary ml-3">Submit</button>
-          </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 mt-2">
+            <button type="submit" class="btn btn-primary ml-3">Submit</button>
+        </div>
 
         </div>
 
